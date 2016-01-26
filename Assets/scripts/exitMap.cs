28ada5ed70694b7eb68e714b4b8198f69717fdd1
scohen18 <sbcohen2000@gmail.com>
2016-01-26 15:41:10 -0700

    Adding Menu Items

---
+++ Assets/scripts/exitMap.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class exitMap : MonoBehaviour {
+
+	public string[] levels;
+	public int levelSelected = 0;
+
+	void Start () {
+		levels = new string[10];
+
+		levels [0] = "gameScene";
+		levels [1] = "field1";
+		levels [2] = "field2";
+		levels [3] = "store";
+		levels [4] = "storage";
+
+	}
+	
+
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider col) {
+	
+		Debug.Log ("hit");
+
+		if(col.gameObject.name == "combineController")
+		{
+			Debug.Log ("combine entered");
+		}
+		if(col.gameObject.name == "tractorController")
+		{
+			Debug.Log ("tractor entered");
+		}
+		if(col.gameObject.name == "truckController")
+		{
+			Debug.Log ("truck entered");
+		}
+	}
+}
--- Assets/scripts/vehicleSelected.cs
+++ Assets/scripts/vehicleSelected.cs
@@ -6,26 +6,39 @@ public class vehicleSelected : MonoBehaviour {
 
 	public static string ID = "";
 	public string[] vehicleList;
-
 	public GameObject pin;
-
 	public static Vector3[] waypoints;
-
 	public Text vehicleSelectedText;
-
 	public GameObject[] pins;
+
+	public Button erasePins;
+	public Button selectCombine;
+	public Button selectTruck;
+	public Button selectTractor;
+
+	public Button none;
+
+	public static bool deletePins = false;
 
 	int i = 0;
 	int j = 0;
 	void Start () {
-		vehicleList = new string[3];
+		vehicleList = new string[4];
 
 		vehicleList [0] = "combine";
 		vehicleList [1] = "truck";
 		vehicleList [2] = "tractor";
+		vehicleList [3] = "none";
 
 		waypoints = new Vector3[10];
 		pins = new GameObject[10];
+
+
+		erasePins.onClick.AddListener(delegate() { destroyPins(); });
+		selectCombine.onClick.AddListener(delegate() {i = 0; });
+		selectTruck.onClick.AddListener(delegate() {i = 1; });
+		selectTractor.onClick.AddListener(delegate() {i = 2; });
+		none.onClick.AddListener(delegate() {i = 3; });
 
 
 	}
@@ -33,7 +46,7 @@ public class vehicleSelected : MonoBehaviour {
 	{
 		if (Input.GetKeyDown (KeyCode.Tab)) {
 			i ++;
-			if(i == 3)
+			if(i == 4)
 			{
 				i = 0;
 			}
@@ -43,7 +56,7 @@ public class vehicleSelected : MonoBehaviour {
 		}
 		ID = vehicleList[i];
 		vehicleSelectedText.text = "controlling " + ID;
-		Debug.Log (j);
+
 		if (j == 10) {
 
 			destroyPins();
@@ -56,6 +69,13 @@ public class vehicleSelected : MonoBehaviour {
 
 				j ++;
 
+				if(deletePins)
+				{
+					j = 0;
+					deletePins = false;
+					destroyPins();
+				}
+
 				RaycastHit hit;
 				Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 				if (Physics.Raycast(ray, out hit))
@@ -64,6 +84,7 @@ public class vehicleSelected : MonoBehaviour {
 					{
 						waypoints[j] = hit.point;
 						Instantiate(pin, hit.point, Quaternion.identity);
+						//Debug.Log("instinated pin " + j + " @ " + waypoints[j].x + "," + waypoints[j].x + "," + waypoints[j].x);
 					}
 				}
 			}
@@ -76,6 +97,7 @@ public class vehicleSelected : MonoBehaviour {
 		for(int k = 0 ; k < pins.Length ; k ++)
 		{
 			Destroy(pins[k]);
+			waypoints[k] = new Vector3(0,0,0);
 		}
 	}
 	
